b5a99237abd1e8bf3b4f1115d05b1bdbd5d88438
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2021-07-11 17:49:03 +0900

    SampleStaticLibrary → SampleStaticLibraryA

---
+++ Include/SampleStaticLibraryA.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace SampleStaticLibraryA {
+
+class SampleStaticLibraryA {
+public:
+
+	static void HelloWorld();
+
+};
+
+};
---
+++ Source/SampleStaticLibraryA.cpp
@@ -0,0 +1,10 @@
+#include <SampleStaticLibraryA.h>
+#include <iostream>
+
+namespace SampleStaticLibraryA {
+
+void SampleStaticLibraryA::HelloWorld() {
+	std::cout << "Hello, world! This is SampleStaticLibraryA!!" << std::endl;
+}
+
+}
